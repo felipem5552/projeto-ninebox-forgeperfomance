550041c1a9041691c1b29ef4a459ad982eb560e0
felipem5552 <felipemendes5552@gmail.com>
2026-01-15 08:27:13 -0300

    docs: Pequenas mudanças na documentação

--- Back-end_REST_API_Express_e_sqlite/src/repositories/ava_repository.ts
+++ Back-end_REST_API_Express_e_sqlite/src/repositories/ava_repository.ts
@@ -1,48 +1,114 @@
+// Métodos de requisição HTTP para as avaliações.
+
 import Pergunta from "../models/pergunta";
 import Modelo_de_Avaliacao from "../models/modelo_de_avaliacao";
 import Instancia_de_Avaliacao from "../models/instancia_de_avaliacao";
 import database from "./database";
+
 const AvaliacaoRepository = {
+
+    // Método de requisição HTTP POST.
+    /**
+     * @param { Modelo_de_Avaliacao } avaliacao - Busca por avaliações, deve ser aguardado pela função callback.
+     * @param { void } callback - Função callback que cria nova avaliação.
+     */
     criarAvaliacao: (avaliacao: Modelo_de_Avaliacao, callback: (id?: number) => void) => {
         const sql = 'INSERT INTO Avaliacoes (titulo) VALUES (?)'
+        
+        // Parâmetro para busca (título). 
         const params = [avaliacao.titulo]
+
         database.run(sql, params, function(_err) {
             callback(this?.lastID)
         })
     },
+
+    // Método de requisição HTTP PUT.
+    /**
+     * 
+     * @param { number } id - Busca pelo ID da avaliação, espera pela função callback.
+     * @param { Modelo_de_Avaliacao } avaliacao - Busca pela avaliação, espera pela função callback.
+     * @param { void } callback - Função callback que retorna se a avaliação existe ou não.
+     */    
     alterarAvaliacao: (id: number, avaliacao: Modelo_de_Avaliacao, callback: (notFound: boolean) => void) => {
     const sql = 'UPDATE avaliacoes SET titulo = ? WHERE id = ?'
+
+    // Parâmetro de modelo (Como deve ser o corpo da avaliação).
     const params = [avaliacao.titulo, id]
+
     database.run(sql, params, function(_err) {
         callback(this.changes === 0)
     })
     },
+
+    // Método de requisição HTTP GET.
+    /**
+     * 
+     * @param { number } Id_Avaliacao - ID da avaliação para busca, deve esperar a função callback
+     * @param { void } callback - Função callback que retorna perguntas
+     */
     verAvaliacao: (Id_Avaliacao: number, callback: (perguntas: Pergunta[]) => void) => {
         const sql = 'SELECT * FROM Perguntas WHERE Modelo = ?'
+
+        // Parâmetro de busca (ID).
         const params = [Id_Avaliacao]
+
         database.all(sql, params, (_err, rows: Pergunta[]) => {callback(rows)})
     },
+
+    // Método de requisição HTTP POST.
+    /**
+     * 
+     * @param { number } modelo - 
+     * @param { Pergunta } pergunta - Busca 
+     * @param { void } callback - Função callback que retorna perguntas
+     */
     inserirPerguntas: (modelo: number, pergunta: Pergunta, callback: (id?: number) => void) => {
         const sql = 'INSERT INTO Perguntas (enunciado, eixo, peso, modelo, disponibilidade) VALUES (?, ?, ?, ?, ?)'
+
+        // Parâmetro de modelo (Como deve ser o corpo da pergunta)
         const params = [pergunta.enunciado, pergunta.eixo, pergunta.peso, modelo, 1]
+
         database.run(sql, params, function(_err) {
             callback(this?.lastID)
         })
     },
+
+    // Método de requisição HTTP PUT.
+    /**
+     * 
+     * @param id 
+     * @param pergunta 
+     * @param callback 
+     */
     alterarPergunta: (id: number, pergunta: Pergunta, callback: (notFound: boolean) => void) => {
     const sql = 'UPDATE perguntas SET enunciado = ?, eixo = ?, peso = ?, modelo = ?, disponibilidade = ? WHERE id = ?'
+
+    // Parâmetro de modelo (Como deve ser o corpo da pergunta)
     const params =  [pergunta.enunciado, pergunta.eixo, pergunta.peso, pergunta.modelo, 1, id]
+
     database.run(sql, params, function(_err) {
         callback(this.changes === 0)
     })
     },
+
+    // Método de requisição HTTP DELETE.
+    /**
+     * 
+     * @param id 
+     * @param callback 
+     */    
     apagarPergunta_Real: (id: number, callback: (notFound: boolean) => void) => {
     const sql = 'DELETE FROM Perguntas WHERE id = ?'
+
+    // Parâmetro para busca (ID)
     const params = [id]
+
     database.run(sql, params, function(_err) {
         callback(this.changes === 0)
     })
     },
+
     Avaliar(
     instancia: Instancia_de_Avaliacao,
     callback: (erro: boolean) => void
@@ -174,3 +240,4 @@ const AvaliacaoRepository = {
 
 }
 export default AvaliacaoRepository
+// Disponibiliza as requisições
