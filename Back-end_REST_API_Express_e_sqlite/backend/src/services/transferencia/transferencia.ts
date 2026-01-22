92925223f8ad4557a33214b8e5a4ae920d3b3dc8
felipem5552 <felipemendes5552@gmail.com>
2026-01-22 15:32:20 -0300

    Finalizada função de planilhas

--- Back-end_REST_API_Express_e_sqlite/backend/src/services/transferencia/transferencia.ts
+++ Back-end_REST_API_Express_e_sqlite/backend/src/services/transferencia/transferencia.ts
@@ -1,25 +1,45 @@
-import database from '../../repositories/database'
+import sqlite3 from 'sqlite3'
 import * as xlsx from 'xlsx';
-import Funcionario from '../../models/funcionario';
+import type FuncionarioRow from '../../repositories/funcionarios_repository';
+
+type FuncionarioRow = {
+  id: number
+  nome: string
+  email: string
+  cargo?: string | null
+  time_id: number
+  time_nome?: string
+  privilegios: 'FUNCIONARIO' | 'GESTOR' | 'ADMIN'
+  senha?: string | null
+  ativo: number
+  data_de_ingresso: string
+}
 
 
 
+const DBSOURCE = "../../../db.sqlite"
+const database = new sqlite3.Database(DBSOURCE, err => {
+  if (err) {
+    console.error('Erro ao conectar no banco:', err.message)
+    throw err
+  }
+})
 
-// 3. Ler o Excel e realizar a inserção
 function importarDadosDoExcel() {
-    // Carrega o arquivo e pega a primeira aba
     const arquivo = xlsx.readFile('dados.xlsx');
     const abaNome = arquivo.SheetNames[0];
-    const dadosJson: Funcionario[] = xlsx.utils.sheet_to_json(arquivo.Sheets[abaNome]);
+    const dadosJson: FuncionarioRow[] = xlsx.utils.sheet_to_json(arquivo.Sheets[abaNome]);
 
     console.log(`Total de linhas detectadas: ${dadosJson.length}`);
 
     database.serialize( () => {database.run('BEGIN TRANSACTION')
-        const stmt = database.prepare(`INSERT INTO funcionarios (nome, time, email, cargo, privilegios,) 
-        VALUES (?, ?, ?, ?, ?)`)
+        const stmt = database.prepare(`INSERT INTO funcionarios (nome, cargo, time_id, email, senha, privilegios) 
+        VALUES (?, ?, ?, ?, ?, ?)`)
         let erro = false
         dadosJson.forEach( (funcionario) => {
-            stmt.run(funcionario.nome, funcionario.time, funcionario.email, funcionario.cargo, funcionario.privilegios,
+            console.log(funcionario)
+            stmt.run(funcionario.nome, funcionario.cargo, funcionario.time_id, funcionario.email, funcionario.senha, 
+                funcionario.privilegios,
                 function (e: Error) {
                     if (e) {
                         erro = true
@@ -27,14 +47,17 @@ function importarDadosDoExcel() {
                 }
             )
         })
-        if (erro) {
-            database.run('ROLLBACK')
-            console.log("Rollback")
-        }
-        else {
-            database.run('COMMIT')
-            console.log("commit")
-        }
+        stmt.finalize( (err) => {
+            if (erro || err) {
+                database.run('ROLLBACK')
+                console.log("Rollback")
+            }
+            else {
+                database.run('COMMIT')
+                console.log("commit")
+            }
+
+        })
     })
 }
 
